b9ea37f89eb7197f297f799a6134b09d60527821
RobinCuevas <37147631+RobinCuevas@users.noreply.github.com>
2020-03-03 11:30:03 -0300

    adding files

---
+++ server.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2018 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+const express        = require('express');
+const app            = express();
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+const config = require('./config');
+
+const port = config.APP.port; // set our port
+
+const publicRoot = __dirname + '/public/';
+
+// parse application/json
+app.use(bodyParser.json());
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// override with the X-HTTP-Method-Override header in the request
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// set static files location
+app.use(express.static(publicRoot));
+
+// pass application into routes
+require('./app/routes')(app, publicRoot);
+
+//start update cron job
+require('./app/jobs/update');
+
+// start app
+app.listen(port);
+console.log('Watson Stock Server starting on port ' + port);
+exports = module.exports = app;
